257687794591c579703e807f637f03c659cfc9d8
brain545 <brain545@yandex.ru>
2017-08-11 23:01:46 +0300

    Assets configured

---
+++ Assets/TextController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+
+	void Start() {
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			text.text = "Hello";
+		}
+	}
+}
