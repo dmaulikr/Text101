36bc729cdbd0af1166f4bb0f56146bf112849a27
brain545 <brain545@yandex.ru>
2017-08-19 15:04:07 +0300

    New version

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -3,15 +3,54 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class TextController : MonoBehaviour {
-
+	
 	public Text text;
+	
+	private enum State
+	{
+		Cell,
+		CellMirror,
+		Mirror,
+		Sheets0,
+		Sheets1,
+		Lock0,
+		Lock1,
+		Freedom
+	}
+	private State curState;
 
 	void Start() {
+		curState = State.Cell;
 	}
 
 	void Update() {
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			text.text = "Hello";
+		switch (curState) {
+		default:
+			break;
+		case State.Cell:    StateCell();    break;
+		case State.Sheets0: StateSheets0(); break;
+		}
+	}
+
+	void StateCell() {
+		text.text =
+			"You are in a prison cell, and you want to escape. There are " +
+			"some dirty sheets on the bed, a mirror on the wall, and the door " +
+			"is locked from outside.\n\n" +
+			"Press S to view Sheets, M to view Mirror or L to view Lock.";
+		if (Input.GetKeyDown(KeyCode.S)) {
+			curState = State.Sheets0;
+		}
+	}
+
+	void StateSheets0() {
+		text.text =
+			"You can't believe you sleep in these things. Surely it's " +
+			"time somebody changed them. The pleasures of prison life " +
+			"I guess!\n\n" +
+			"Press R to Return to roaming your cell.";
+		if (Input.GetKeyDown(KeyCode.R)) {
+			curState = State.Cell;
 		}
 	}
 }
