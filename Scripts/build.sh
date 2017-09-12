48b7da41120ba5cd273f23e3f26ecaa4d0e5b0ef
highflyer <vladimir.ananyev@regium.com>
2017-09-12 08:44:20 +0300

    Complete CI scripts

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,3 +1,16 @@
 #! /bin/sh
 
-echo "Build"
+project="Text101"
+
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
+  -quit
+
+echo "Logs from build"
+cat $(pwd)/unity.log
